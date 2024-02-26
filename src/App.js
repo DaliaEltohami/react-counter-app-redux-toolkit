8a58bd8b3dc64604399eb34e7145cd660073cca8
dalia eltohami <daliaeltohami1991@gmail.com>
2024-02-26 03:25:58 +0200

    upload app files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import CounterReduxToolkit from "./components/CounterReduxToolkit";
+
+function App() {
+  return (
+    <div className="App">
+      React Counter App "with and without Redux"
+      <CounterReduxToolkit />
+    </div>
+  );
+}
+
+export default App;
